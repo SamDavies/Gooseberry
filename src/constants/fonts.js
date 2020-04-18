83943477f54df06536275bb078c1f9d7248883f4
Sam Davies <samdavies@live.co.uk>
2020-04-18 18:05:04 +0100

    Fix heading margin

--- src/constants/fonts.js
+++ src/constants/fonts.js
@@ -1,6 +1,6 @@
 const fonts = {
     serifFont: 'Georgia, Cambria, "Times New Roman", Times, serif',
-    sansSerifFont: '-apple-system, BlinkMacSystemFont, sans-serif'
+    sansSerifFont: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";'
 }
 
 export default fonts
